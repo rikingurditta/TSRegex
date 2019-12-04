86ce4021c2abf7875e30fdc430401521d00f6c7b
Rikin <dolllyamar@gmail.com>
2019-12-04 12:42:06 -0500

    organized file structure of repo

---
+++ regex.js
@@ -0,0 +1,115 @@
+var EPSILON = "ε";
+
+// abstract class for parsed regular expressions
+class Regex {
+	// get the NFA which accepts the strings matched by this regex
+	getNFA() {
+		throw new Error('Not implemented!');
+	}
+}
+
+
+// parsed regular expression for a single symbol
+class RESymbol extends Regex {
+	// create a new symbol regex
+	constructor(symbol) {
+		super();
+		if (symbol.length != 1) {
+			throw Error("Not a symbol!");
+		}
+		this.symbol = symbol;
+	}
+
+	// get the NFA which accepts this regex's symbol 
+	getNFA() {
+		let reject = new NFAState(this.symbol + "0", {}, false);
+		let accept = new NFAState(this.symbol + "1", {}, true);
+		reject.addTransition(this.symbol, accept);
+		let states = new NSet([reject, accept])
+		return new NFA(reject, states);
+	}
+}
+
+
+// parsed regular expression for a sequence of regexes
+class RESequence extends Regex {
+	// create a new sequence of regexes
+	constructor(reList) {
+		super();
+		this.subs = reList;
+	}
+
+	// get the NFA which accepts this sequence of regexes
+	getNFA() {
+		if (this.subs.length == 0) {
+			let s = new NFAState("empty sequence", {}, true);
+			return new NFA(s, new NSet([s]));
+		}
+		// concatenate all NFAs in order to make NFA which accepts sequence
+		let start = this.subs[0].getNFA();
+		for (let i = 1; i < this.subs.length; i += 1) {
+			start.append(this.subs[i].getNFA());
+		}
+		return start;
+	}
+}
+
+
+// parsed regular expression for applying {num} to a regex,
+// aka repeating a regex num times
+class RERepeat extends Regex {
+	// create a new repeat regex
+	constructor(regex, num) {
+		super();
+		this.inside = regex;
+		this.num = num;
+	}
+
+	// get the NFA which accepts num repetitions of the inside regex
+	getNFA() {
+		let reList = [];
+		for (int i = 0; i < this.num; i += 1) {
+			reList.push(this.inside);
+		}
+		return new RESequence(reList);
+	}
+}
+
+
+// parsed regular expression for applying * to a regex,
+class REStar extends Regex {
+	// create a new star regex
+	constructor(regex) {
+		super();
+		this.inside = regex;
+	}
+
+
+	// get the NFA which accepts arbitrary repetitions of the inside regex
+	getNFA() {
+		let start = new NFAState("*start", {}, true);
+		let out = new NFA(start, new NSet([start]));
+		let insideNFA = this.inside.getNFA();
+		insideNFA.getAcceptingStates().forEach(function(state) {
+			state.addTransition(insideNFA.start)
+		});
+		out.append(insideNFA);
+		return out;
+	}
+}
+
+
+// parsed regular expression for applying * to a regex
+class REPlus extends Regex {
+	// create a new star regex
+	constructor(regex) {
+		super();
+		this.inside = regex;
+	}
+
+
+	// get the NFA which accepts arbitrary repetitions of the inside regex
+	getNFA() {
+		return new RESequence([this.inside, new REStar(this.inside)]).getNFA();
+	}
+}
